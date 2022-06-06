d0d38ec5b207e97b9e570d2082de4020e9166bcf
Juan G. Vargas Jr <18316297+jgvargas@users.noreply.github.com>
2022-06-06 17:51:00 -0500

    Fixed CMS linking

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
   // Grab css and imgs, src to public folder
   eleventyConfig.addPassthroughCopy('./src/style.css')
   eleventyConfig.addPassthroughCopy('./src/assets')
+  eleventyConfig.addPassthroughCopy('./src/admin')
 
   eleventyConfig.addFilter('postDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
